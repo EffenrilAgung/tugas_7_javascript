7b59b2720e146c6a4a80fcac37e10106bd9b11cb
EffenrilAgung <66568136+EffenrilAgung@users.noreply.github.com>
2021-06-01 20:44:31 +0700

    Initial commit

---
+++ tugas7.js
@@ -0,0 +1,11 @@
+var tinggiBadan = [180, 160, 165, 167, 170, 150, 166, 169, 168, 175]
+for (x = 0; x < tinggiBadan.length; x++) {
+    console.log(tinggiBadan[x])
+}
+console.log('========= let 0f =========')
+for (let y of tinggiBadan) {
+    console.log(y)
+}
+/*
+Perbedaannya adalah kita dapat menampilkan variable array tanpa harus memanggil variable tinggiBadan karena sudah di gantikan dengan variable Y
+*/
